4f05acfc1be04dad914f7140671ce8807dd8f845
LordMsz <33070601+LordMsz@users.noreply.github.com>
2021-10-08 00:08:54 +0200

    Beba experiment hra

--- main.ts
+++ main.ts
@@ -10,29 +10,77 @@ function lightMenuItem (num: number) {
     led.plot(num % 5, num / 5)
 }
 function showImage (num: number) {
+    music.stopAllSounds()
     if (num == 0) {
         basic.showIcon(IconNames.Giraffe)
+        soundExpression.giggle.play()
     }
     if (num == 1) {
         basic.showIcon(IconNames.Tortoise)
+        soundExpression.mysterious.play()
     }
     if (num == 2) {
         basic.showIcon(IconNames.Angry)
+        soundExpression.hello.play()
     }
     if (num == 3) {
         basic.showIcon(IconNames.Happy)
+        soundExpression.happy.play()
     }
-    if (num >= 4) {
+    if (num == 4) {
+        basic.showIcon(IconNames.Sad)
+        soundExpression.sad.play()
+    }
+    if (num == 5) {
+        basic.showIcon(IconNames.Umbrella)
+        soundExpression.slide.play()
+    }
+    if (num == 6) {
+        basic.showIcon(IconNames.StickFigure)
+        soundExpression.soaring.play()
+    }
+    if (num == 7) {
+        basic.showIcon(IconNames.Duck)
+        soundExpression.spring.play()
+    }
+    if (num == 8) {
+        soundExpression.twinkle.play()
+        for (let index = 0; index < 4; index++) {
+            basic.showIcon(IconNames.Diamond)
+            basic.pause(250)
+            basic.showIcon(IconNames.Target)
+            basic.pause(250)
+            basic.showIcon(IconNames.SmallDiamond)
+            basic.pause(250)
+        }
+    }
+    if (num == 9) {
+        basic.showIcon(IconNames.EigthNote)
+        soundExpression.yawn.play()
+    }
+    if (num == 10) {
+        basic.showIcon(IconNames.Snake)
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+    }
+    if (num == 11) {
+        basic.showIcon(IconNames.Rollerskate)
+        music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Forever)
+    }
+    if (num == 12) {
+        basic.showIcon(IconNames.Yes)
+        music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+    }
+    if (num >= 13) {
         basic.showIcon(IconNames.House)
-        music.playMelody("C F A C5 C5 A C5 C5 ", 480)
+        soundExpression.sad.play()
+        basic.pause(250)
+        currentMenuItem = 0
+        showImage(currentMenuItem)
     }
 }
 input.onButtonPressed(Button.A, function () {
     if (currentApp == -1) {
         listMenu(-1)
-    }
-    if (currentApp == 2) {
-        runAppChartFill(-5)
     }
 })
 function runAppImageSwitcher () {
@@ -61,7 +109,7 @@ function listMenu (delta: number) {
             pořadí,
             100
             )
-            basic.pause(4)
+            basic.pause(6)
         }
         currentMenuItem = 0
     }
@@ -69,21 +117,23 @@ function listMenu (delta: number) {
         currentMenuItem = 24
     }
     basic.clearScreen()
-    lightMenuItem(currentMenuItem)
+    showImage(currentMenuItem)
 }
 input.onButtonPressed(Button.AB, function () {
-    if (currentApp == -1) {
-        currentMenuItem = 0
+    images.createBigImage(`
+        . . # . . . # . . .
+        . . . . . . . . . .
+        . # . # . # . # . #
+        . . . . . . . . . .
+        # . . . # . . . # .
+        `).scrollImage(1, 200)
+    for (let index = 0; index < 4; index++) {
+        music.playMelody("C E G B C5 B G E ", 333)
     }
-    currentApp = -1
-    listMenu(0)
 })
 input.onButtonPressed(Button.B, function () {
     if (currentApp == -1) {
         listMenu(1)
-    }
-    if (currentApp == 2) {
-        runAppChartFill(5)
     }
 })
 input.onGesture(Gesture.Shake, function () {
@@ -116,4 +166,3 @@ currentMenuItem = 0
 currentApp = -1
 music.setVolume(100)
 music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.OnceInBackground)
-listMenu(0)
